d2652b5a73b8ba0f665d2b83fc84711f319a5595
Daniel Hurtado <a01707774@tec.mx>
2025-05-23 17:06:51 -0600

    feat: add Lexer and main test execution script

---
+++ main.py
@@ -0,0 +1,28 @@
+from Lexer import *
+from Parser import *
+from Translator import *
+import sys
+
+if __name__ == "__main__":
+  print("----GOOD TEST CASES----")
+  for i in range(7):
+    parser = Parser(f"test_cases/good/prog{i + 1}.txt")
+    tree = parser.analize()
+
+    if tree != None:
+      tree.eval()
+
+    print(f"Test #{i + 1}: END")
+
+  print("----BAD TEST CASES----")
+  for i in range(7):
+    try:
+      parser = Parser(f"test_cases/bad/prog{i + 1}.txt")
+      tree = parser.analize()
+
+      if tree != None:
+        tree.eval()
+
+      print(f"Test #{i + 1} Passed <-- Shouln't pass")
+    except Exception as e:
+      print(f"Test #{i + 1} failed: {e}")
