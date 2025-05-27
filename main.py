01ab596b612a8bfc5a4a7492d7611a8763df9930
Daniel Hurtado <a01707774@tec.mx>
2025-05-27 16:27:57 -0600

    refactor: streamline main execution flow by removing redundant test case loops

--- main.py
+++ main.py
@@ -4,25 +4,8 @@ from Translator import *
 import sys
 
 if __name__ == "__main__":
-  print("----GOOD TEST CASES----")
-  for i in range(1):
-    parser = Parser(f"test_cases/good/prog{i + 1}.txt")
-    tree = parser.analize()
+  parser = Parser("test_cases/good/prog1.txt")
+  tree = parser.analize()
 
-    if tree != None:
-      tree.eval()
-
-    print(f"Test #{i + 1}: END")
-
-  print("----BAD TEST CASES----")
-  for i in range(1):
-    try:
-      parser = Parser(f"test_cases/bad/prog{i + 1}.txt")
-      tree = parser.analize()
-
-      if tree != None:
-        tree.eval()
-
-      print(f"Test #{i + 1} Passed <-- Shouln't pass")
-    except Exception as e:
-      print(f"Test #{i + 1} failed: {e}")
+  if tree != None:
+    tree.eval()
